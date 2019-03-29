44fcf10dcb8370593e1ed69bc3c49e5b1df4b8a4
Jason Modisett <jason@Jasons-MacBook-Pro.local>
2019-03-29 07:25:10 -0500

    Finished iterative and recursive

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -2,14 +2,31 @@
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
-    # TO-DO
-    
-    return merged_arr
 
+    a = 0
+    b = 0
+    for i in range(0, elements):
+        if a >= len(arrA):
+            merged_arr[i] = arrB[b]
+            b += 1
+        elif b >= len(arrB):
+            merged_arr[i] = arrA[a]
+            a += 1
+        elif arrA[a] < arrB[b]:
+            merged_arr[i] = arrA[a]
+            a += 1
+        else:
+            merged_arr[i] = arrB[b]
+            b += 1
+
+    return merged_arr
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
+    if len(arr) > 1:
+        left = merge_sort(arr[0: len(arr) // 2])
+        right = merge_sort(arr[len(arr) // 2:])
+        arr = merge(left, right)
 
     return arr
 
@@ -31,3 +48,12 @@ def merge_sort_in_place(arr, l, r):
 def timsort( arr ):
 
     return arr
+
+
+arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+arr2 = []
+arr3 = [2]
+arr4 = [0, 1, 2, 3, 4, 5]
+arr5 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+
+print(merge_sort(arr5))
